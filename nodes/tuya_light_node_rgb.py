d95cbe31bedc725d1322ab5143ea6e681684737c
sjpbailey <sjpbailey1961@gmail.com>
2023-07-02 16:22:26 -0700

    Update tuya_light_node_rgb.py

--- nodes/tuya_light_node_rgb.py
+++ nodes/tuya_light_node_rgb.py
@@ -330,7 +330,7 @@ class LightNode(udi_interface.Node):
 
         def set_percent(self, command):
             percent = int(command.get('value')*10)
-        if percent < 1 or percent > 100:
+        if percent < 1 or percent > 1000:
             LOGGER.error('Invalid Level {}'.format(percent))
         else:
             new_bright = percent
@@ -345,29 +345,6 @@ class LightNode(udi_interface.Node):
                 LOGGER.info(new_colour)
                 # Set colour ✨
                 bulb.set_colour_v2(new_colour)
-        
-        #whi_vlv = 10
-        #bulb.set_bright_v2(whi_vlv)
-        #new_colour = [255, 0, 0] 
-                
-        """openapi = TuyaOpenAPI(API_ENDPOINT, ACCESS_ID, ACCESS_KEY)
-        openapi.connect()
-        self.SwStat(self)
-
-        ivr_one = 'percent'
-        percent = int(command.get('value'))
-
-        def set_percent(self, command):
-            percent = int(command.get('value')*10)
-        if percent < 1 or percent > 100:
-            LOGGER.error('Invalid Level {}'.format(percent))
-        else:
-            commands = {'commands': [
-                {'code': 'bright_value_v2', 'value': int(percent)*10}]}
-            openapi.post(
-                '/v1.0/iot-03/devices/{}/commands'.format(DEVICELED_ID), commands)
-            self.setDriver('GV3', percent)
-            LOGGER.info('Dimmer Setpoint = ' + str(percent) + ' Level')"""
 
     def SwStat(self, command):
         API_ENDPOINT = self.API_ENDPOINT
@@ -422,127 +399,3 @@ class LightNode(udi_interface.Node):
         'STLVL': setDim,
         'QUERY': query,
     }
-
-
-    # Test Light
-    """def setclrflip(self, command):
-        API_ENDPOINT = self.API_ENDPOINT
-        ACCESS_ID = self.ACCESS_ID
-        ACCESS_KEY = self.ACCESS_KEY
-        DEVICELED_ID = self.DEVICELED_ID
-        openapi = TuyaOpenAPI(API_ENDPOINT, ACCESS_ID, ACCESS_KEY)
-        openapi.connect()
-        self.SwStat(self)
-
-        # On Off Test
-        LOGGER.info('\nSwitch Test')
-        commands = {'commands': [{'code': 'switch_led', 'value': True}]}
-        openapi.post(
-            '/v1.0/iot-03/devices/{}/commands'.format(DEVICELED_ID), commands)
-        LOGGER.info('On Test')
-        time.sleep(.5)
-        self.SwStat(self)
-        time.sleep(2)
-        commands = {'commands': [{'code': 'switch_led', 'value': False}]}
-        openapi.post(
-            '/v1.0/iot-03/devices/{}/commands'.format(DEVICELED_ID), commands)
-        LOGGER.info('Off Test')
-        time.sleep(.5)
-        self.SwStat(self)
-        time.sleep(2)
-        commands = {'commands': [{'code': 'switch_led', 'value': True}]}
-        openapi.post(
-            '/v1.0/iot-03/devices/{}/commands'.format(DEVICELED_ID), commands)
-        LOGGER.info('On Test')
-        time.sleep(.5)
-        self.SwStat(self)
-        time.sleep(2)
-
-        # Dimmer Test
-        LOGGER.info('\nDimmer Control Test')
-        for level in range(11):
-            LOGGER.info('    Level: %d%%' % (level*10))
-            commands = {'commands': [
-                {'code': 'bright_value_v2', 'value': int(level)*10}]}
-            openapi.post(
-                '/v1.0/iot-03/devices/{}/commands'.format(DEVICELED_ID), commands)
-            self.setDriver('GV3', level)
-            time.sleep(2)
-
-        # Test Modes
-        LOGGER.info('\nTesting Bulb Modes')
-        LOGGER.info('Colour Test')
-        commands = {'commands': [{'code': 'work_mode', 'value': 'colour'}]}
-        openapi.post(
-            '/v1.0/iot-03/devices/{}/commands'.format(DEVICELED_ID), commands)
-        time.sleep(.5)
-        self.SwStat(self)
-        time.sleep(2)
-        LOGGER.info('Scene Test')
-        commands = {'commands': [{'code': 'work_mode', 'value': 'scene'}]}
-        openapi.post(
-            '/v1.0/iot-03/devices/{}/commands'.format(DEVICELED_ID), commands)
-        time.sleep(.5)
-        self.SwStat(self)
-        time.sleep(2)
-        LOGGER.info('Music Test')
-        commands = {'commands': [{'code': 'work_mode', 'value': 'music'}]}
-        openapi.post(
-            '/v1.0/iot-03/devices/{}/commands'.format(DEVICELED_ID), commands)
-        time.sleep(.5)
-        self.SwStat(self)
-        time.sleep(2)
-        LOGGER.info('White Test')
-        commands = {'commands': [{'code': 'work_mode', 'value': 'white'}]}
-        openapi.post(
-            '/v1.0/iot-03/devices/{}/commands'.format(DEVICELED_ID), commands)
-        time.sleep(.5)
-        self.SwStat(self)
-        time.sleep(2)
-
-        # Color Cycler
-        LOGGER.info('\nColor Test')
-        bulb = Bulb(
-            client_id=self.ACCESS_ID,
-            secret_key=self.ACCESS_KEY,
-            device_id=self.DEVICELED_ID,
-            region_key=self.API_REGION,
-        )
-
-        current_colour = json.loads(bulb.current_value("colour_data_v2"))
-        # Conversion current HSV to RGB
-        current_colour = colorsys.hsv_to_rgb(
-            h=current_colour["h"] / 360,
-            s=current_colour["s"] / 1000,
-            v=current_colour["v"] / 1000,
-        )
-        # Convert the current RGB to format 0-255
-        current_colour = tuple(map(lambda x: int(x * 255), current_colour))
-        LOGGER.info(current_colour)
-        # Get new RGB colour
-        # colorchooser.askcolor(color=current_colour)[0]
-        rainbow = {"red": [255, 0, 0], "orange": [255, 127, 0], "yellow": [255, 200, 0], "green": [
-            0, 255, 0], "blue": [0, 0, 255], "indigo": [46, 43, 95], "violet": [139, 0, 255], "white": [255, 255, 255]}
-        for x in range(2):
-            for i in rainbow:
-                r = rainbow[i][0]
-                g = rainbow[i][1]
-                b = rainbow[i][2]
-                time.sleep(2)
-                LOGGER.info('    %s (%d,%d,%d)' % (i, r, g, b))
-                new_colour = (r, g, b)
-                # Convert RGB coordinates to int
-                new_colour = tuple(map(lambda x: int(x), new_colour))
-                LOGGER.info(new_colour)
-                # Set colour ✨
-                bulb.set_colour_v2(new_colour)
-            else:
-                LOGGER.info('Test Complete')
-                return
-        time.sleep(1)
-        commands = {'commands': [{'code': 'work_mode', 'value': 'white'}]}
-        openapi.post(
-            '/v1.0/iot-03/devices/{}/commands'.format(DEVICELED_ID), commands)
-        time.sleep(.5)
-        self.SwStat(self)"""        
-
