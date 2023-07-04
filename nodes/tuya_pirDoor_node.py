f2a6772f886ed053dcde9ffb1602b60fcbe9209f
sjpbailey <sjpbailey1961@gmail.com>
2023-07-04 10:12:48 -0700

    Update tuya_pirDoor_node.py

--- nodes/tuya_pirDoor_node.py
+++ nodes/tuya_pirDoor_node.py
@@ -35,7 +35,6 @@ class PirDNode(udi_interface.Node):
         self.apiEndpoint = apiEndpoint
         self.API_ENDPOINT = apiEndpoint
         self.name = name
-        LOGGER.info(name)
         self.setDriver('ST', 1)
         
 
@@ -51,6 +50,7 @@ class PirDNode(udi_interface.Node):
             "/v1.0/iot-03/devices/{}".format(DEVICESW_ID) + "/status/")
         #LOGGER.info(response1)
         for i in response1['result'][1:2]:
+            LOGGER.info(self.name)
             LOGGER.info('Battery {}'.format(i['value']))
             self.setDriver('GV3', i['value'])
         for i in response1['result'][0:1]:
