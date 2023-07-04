7d081db43e0c928bca3c9a6632b5f243c314ddb9
sjpbailey <sjpbailey1961@gmail.com>
2023-07-03 22:12:27 -0700

    Update tuya_pirDoor_node.py

--- nodes/tuya_pirDoor_node.py
+++ nodes/tuya_pirDoor_node.py
@@ -54,9 +54,9 @@ class PirDNode(udi_interface.Node):
             self.setDriver('GV3', i['value'])
             
         for i in response1['result'][0:1]:
-            if i['value'] == 'True':
+            if i['value'] == "True":
                 self.setDriver('GV2', 1)
-            elif i['value'] == 'False':
+            elif i['value'] == "False":
                 self.setDriver('GV2', 0)
 
     def poll(self, polltype):
