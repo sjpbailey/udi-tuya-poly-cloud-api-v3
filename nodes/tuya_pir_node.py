7570107915cb81537816b98e47855bd40592b7ce
sjpbailey <sjpbailey1961@gmail.com>
2023-07-04 10:16:09 -0700

    Update tuya_pir_node.py

--- nodes/tuya_pir_node.py
+++ nodes/tuya_pir_node.py
@@ -59,8 +59,8 @@ class PirNode(udi_interface.Node):
             "/v1.0/iot-03/devices/{}".format(DEVICESW_ID) + "/status/")
         #LOGGER.info(response1)
         for i in response1['result'][1:2]:
-            LOGGER.info(self.name)
-            LOGGER.info('Battery {}'.format(i['value']))
+            #LOGGER.info(self.name)
+            LOGGER.info(self.name, 'Battery {}'.format(i['value']))
             self.setDriver('GV3', i['value'])
         for i in response1['result'][0:1]:
             if i['value'] == 'pir':
